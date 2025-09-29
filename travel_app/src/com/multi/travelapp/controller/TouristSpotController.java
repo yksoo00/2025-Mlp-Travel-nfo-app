ba98205de00f01e0d3129b691a814da7b4616532
hamaybe00 <gimhayeon38@gmail.com>
2025-09-29 22:47:20 +0900

    feat(TouristService):memberService에 있던 장소 조회 기능 TouristService로 분리

--- travel_app/src/com/multi/travelapp/controller/TouristSpotController.java
+++ travel_app/src/com/multi/travelapp/controller/TouristSpotController.java
@@ -2,17 +2,20 @@ package com.multi.travelapp.controller;
 
 import com.multi.travelapp.model.dto.TouristSpotDto;
 import com.multi.travelapp.service.MemberService;
+import com.multi.travelapp.service.TouristService;
 import com.multi.travelapp.view.TravelView;
 
 import java.util.ArrayList;
 
 public class TouristSpotController {
 
+    private TouristService touristService=new TouristService();
+
     public void selectTouristSpotByTitle(Long memberId, String title) {
         TravelView travelView = new TravelView();
 
         try{
-            ArrayList<TouristSpotDto> list = memberService.selectTourSpotsByTitle(title);
+            ArrayList<TouristSpotDto> list = touristService.selectTourSpotsByTitle(title);
             if (!list.isEmpty()) {
                 travelView.displayRegionTitle(list);
             } else {
@@ -28,15 +31,12 @@ public class TouristSpotController {
 
     }
 
-    private MemberService memberService=new MemberService();
-
-
 
     public void selectTouristSpotByDistrict(Long memberId, String district) {
         TravelView travelView = new TravelView();
 
         try{
-            ArrayList<TouristSpotDto> list = memberService.selectTourSpotsByRegion(district);
+            ArrayList<TouristSpotDto> list = touristService.selectTourSpotsByRegion(district);
 
             if (!list.isEmpty()) {
                 travelView.displayRegionPlace(list);
@@ -57,7 +57,7 @@ public class TouristSpotController {
         ArrayList<TouristSpotDto> list=new ArrayList<>();
 
         try{
-             list = memberService.selectTouristSpotById(memberId,touristSpotId);
+             list = touristService.selectTouristSpotById(memberId,touristSpotId);
 
             if (!list.isEmpty()) {
                 travelView.detailPage(memberId,touristSpotId,list);
--- travel_app/src/com/multi/travelapp/service/TouristService.java
+++ travel_app/src/com/multi/travelapp/service/TouristService.java
@@ -1,4 +1,91 @@
 package com.multi.travelapp.service;
 
+import com.multi.travelapp.common.DBConnectionMgr;
+import com.multi.travelapp.model.dao.TouristSpotDao;
+import com.multi.travelapp.model.dto.TouristSpotDto;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class TouristService {
+
+    private final TouristSpotDao touristSpotDao;
+    Connection conn=null;
+    DBConnectionMgr dbcp=null;
+
+    public TouristService() {
+        dbcp = DBConnectionMgr.getInstance();
+
+        if (dbcp.getConnectionCount() == 0) {
+            try {
+                dbcp.setInitOpenConnections(10);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        touristSpotDao = new TouristSpotDao();
+    }
+
+    //장소 아이디로 상세정보 조회
+    public ArrayList<TouristSpotDto> selectTouristSpotById(Long memberId, Long touristSpotId) {
+        ArrayList<TouristSpotDto> list = new ArrayList<>();
+        Connection conn = null;
+
+        try {
+            conn = dbcp.getConnection();
+            list = touristSpotDao.selectTourSpotsById(conn, memberId,touristSpotId);
+        } catch (Exception e) {
+            throw new RuntimeException("상세정보 조회 실패", e);
+        } finally {
+            if (conn != null) {
+                dbcp.freeConnection(conn);
+            }
+        }
+
+        return list;
+
+    }
+
+    //제목별 장소조회
+    public ArrayList<TouristSpotDto> selectTourSpotsByTitle(String title) {
+        ArrayList<TouristSpotDto> list = new ArrayList<>();
+        Connection conn = null;
+
+        try {
+            conn = dbcp.getConnection();
+            list = touristSpotDao.selectTourSpotsByTitle(conn, title);
+        } catch (Exception e) {
+            throw new RuntimeException("제목별 관광지 조회 실패", e);
+        } finally {
+            if (conn != null) {
+                dbcp.freeConnection(conn);
+            }
+        }
+
+        return list;
+    }
+
+    //권역별 관광지 조회
+    public ArrayList<TouristSpotDto> selectTourSpotsByRegion(String district) {
+        ArrayList<TouristSpotDto> list = new ArrayList<>();
+        Connection conn = null;
+
+        try {
+            conn = dbcp.getConnection();
+            list = touristSpotDao.selectTourSpotsByRegion(conn, district);
+        } catch (Exception e) {
+            throw new RuntimeException("권역별 관광지 조회 실패", e);
+        } finally {
+            if (conn != null) {
+                dbcp.freeConnection(conn);
+            }
+        }
+
+        return list;
+
+    }
+
+
 }
