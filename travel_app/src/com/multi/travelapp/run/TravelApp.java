3a163d3c9bc101a3650e7d558bbe7bbac10a4f76
Lee Ji Heon <jiheon340@gmail.com>
2025-09-29 17:39:18 +0900

    feat(TravelApp) : 실행하면 firstPage()를 실행하도록 수정

--- travel_app/src/com/multi/travelapp/run/TravelApp.java
+++ travel_app/src/com/multi/travelapp/run/TravelApp.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class TravelApp {
     public static void main(String[] args) {
         TravelView travelView = new TravelView();
+        travelView.firstPage();
 
     }
 }
