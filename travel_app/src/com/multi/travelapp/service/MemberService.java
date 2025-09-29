640788a967e3b175cbf128a4e0010698994fe401
Jeon Sung Gu <jsg307@nate.com>
2025-09-29 16:16:21 +0900

    feat(MemberService) : 회원가입 로그인 Service 구현

--- travel_app/src/com/multi/travelapp/service/MemberService.java
+++ travel_app/src/com/multi/travelapp/service/MemberService.java
@@ -1,4 +1,66 @@
 package com.multi.travelapp.service;
 
+import com.multi.travelapp.common.DBConnectionMgr;
+import com.multi.travelapp.model.dao.MemberDao;
+import com.multi.travelapp.model.dto.MemberDto;
+import com.multi.travelapp.model.dto.SignInDto;
+
+import java.sql.Connection;
+
+import static java.sql.DriverManager.getConnection;
+
 public class MemberService {
+
+    private MemberDao memberDao = new MemberDao();
+    private DBConnectionMgr dbcp = DBConnectionMgr.getInstance();
+
+
+    public boolean signUp(MemberDto memberDto) {
+        boolean result = false;
+        Connection conn = null;
+
+        try {
+            conn = dbcp.getConnection();
+            conn.setAutoCommit(false);
+            int insertResult = memberDao.insertMember(conn, memberDto);
+
+            if(insertResult>0) {
+                conn.commit();
+                result = true;
+            }else {
+                conn.rollback();
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }finally {
+            if (conn != null) {
+                dbcp.freeConnection(conn);
+            }
+        }
+
+        return result;
+
+    }
+
+    public MemberDto signIn(SignInDto signInDto) {
+        Connection conn = null;
+        MemberDto memberDto = null;
+
+        try {
+            conn = dbcp.getConnection();
+
+            memberDto = memberDao.selectMemberByEmail(conn, signInDto.getEmail(), signInDto.getPassword());
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }finally {
+            if (conn != null) {
+                dbcp.freeConnection(conn);
+            }
+        }
+        return memberDto;
+
+    }
 }
