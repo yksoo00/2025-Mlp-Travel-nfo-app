ce6663464a5b80676daa11023e9ab1fff3cdb731
Jeon Sung Gu <jsg307@nate.com>
2025-09-30 13:29:59 +0900

    feat(BookMarkDto) : 즐겨찾기 페이지 구현

--- travel_app/src/com/multi/travelapp/model/dto/BookMarkDto.java
+++ travel_app/src/com/multi/travelapp/model/dto/BookMarkDto.java
@@ -1,4 +1,43 @@
 package com.multi.travelapp.model.dto;
 
 public class BookMarkDto {
+    private int bookmarkId;
+    private int memberId;
+    private int touristSpotId;
+
+    public BookMarkDto() {
+    }
+
+    public int getBookmarkId() {
+        return bookmarkId;
+    }
+
+    public void setBookmarkId(int bookmarkId) {
+        this.bookmarkId = bookmarkId;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public int getTouristSpotId() {
+        return touristSpotId;
+    }
+
+    public void setTouristSpotId(int touristSpotId) {
+        this.touristSpotId = touristSpotId;
+    }
+
+    @Override
+    public String toString() {
+        return "BookMarkDto{" +
+                "bookmarkId=" + bookmarkId +
+                ", memberId=" + memberId +
+                ", touristSpotId=" + touristSpotId +
+                '}';
+    }
 }
