52b39d10a7098a1eb09c8caabb0b4437625cc71a
Jeon Sung Gu <jsg307@nate.com>
2025-09-30 13:29:24 +0900

    feat(BookMarkController) : 즐겨찾기 페이지 구현

--- travel_app/src/com/multi/travelapp/controller/BookMarkController.java
+++ travel_app/src/com/multi/travelapp/controller/BookMarkController.java
@@ -1,4 +1,34 @@
 package com.multi.travelapp.controller;
 
+import com.multi.travelapp.model.dto.BookMarkDto;
+import com.multi.travelapp.model.dto.ReviewDto;
+import com.multi.travelapp.model.dto.TouristSpotDto;
+import com.multi.travelapp.service.BookMarkService;
+
+import com.multi.travelapp.view.TravelView;
+
+import java.util.ArrayList;
+
 public class BookMarkController {
+    private BookMarkService bookMarkService = new BookMarkService();
+
+    public BookMarkController() {
+    }
+
+    public void selectAllMyBookMarkPage(Long memberId) {
+        TravelView travelView = new TravelView();
+
+        try {
+            ArrayList<TouristSpotDto> list = bookMarkService.selectAllBookMarkByMemberId(memberId); // 내 즐겨찾기 전체 가져오기
+
+            if (!list.isEmpty()) {
+                travelView.displayBookMarkList(list);
+            } else {
+                travelView.displayNoData();
+            }
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
